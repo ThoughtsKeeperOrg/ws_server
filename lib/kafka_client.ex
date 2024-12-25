dcac47405e46860032a1e25b679e55599ff3ef5c
isidzukuri <axesigon@gmail.com>
2024-12-25 15:28:39 +0200

    init

---
+++ lib/kafka_client.ex
@@ -0,0 +1,27 @@
+ defmodule KafkaClient do
+  use Broadway
+
+  def start_link(_opts) do
+    Broadway.start_link(__MODULE__,
+      name: __MODULE__,
+      producer: [
+        module: {BroadwayKafka.Producer, [
+          hosts: [localhost: 9092],
+          # group_id: "group_1",
+          topics: ["test"],
+        ]},
+        concurrency: 1
+      ],
+      processors: [
+        default: [
+          concurrency: 10
+        ]
+      ]
+    )
+  end
+
+  def handle_message(_, message, _) do
+    IO.inspect(message.data, label: "Got message")
+    message
+  end
+end
